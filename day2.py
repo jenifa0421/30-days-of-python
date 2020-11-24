7e15ab3845b88f3dc0dbcb954e1f5bb905759146
jenifa0421 <55661029+jenifa0421@users.noreply.github.com>
2020-11-24 13:23:57 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,26 @@
+# 1.Variable
+
+
+a=b=c=4
+print(a/10)
+print(b*50)
+print(c+60)
+# 2.replace
+
+
+text="mango"
+text = text.replace("n","g")
+print(text)
+# 3.var conversion
+
+
+a=4
+b=21.0
+print("before conversion")
+print(a)
+print(b)
+c=float(a)
+d=int(b)
+print("after conversion")
+print(c)
+print(d)
